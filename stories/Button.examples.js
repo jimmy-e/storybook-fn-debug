b6f6452f3519defb8023f60e9cb9c40a8002887b
Jimmy Emery <jimmy@jkbx.com>
2023-03-20 20:29:01 -0400

    updating button stories

---
+++ stories/Button.examples.js
@@ -0,0 +1,26 @@
+export const Primary = {
+  args: {
+    primary: true,
+    label: 'Button',
+  },
+};
+
+export const Secondary = {
+  args: {
+    label: 'Button',
+  },
+};
+
+export const Large = {
+  args: {
+    size: 'large',
+    label: 'Button',
+  },
+};
+
+export const Small = {
+  args: {
+    size: 'small',
+    label: 'Button',
+  },
+};
--- stories/Button.stories.js
+++ stories/Button.stories.js
@@ -1,6 +1,6 @@
 import { Button } from './Button';
+export * from './Button.examples';
 
-// More on how to set up stories at: https://storybook.js.org/docs/7.0/react/writing-stories/introduction
 export default {
   title: 'Example/Button',
   component: Button,
@@ -10,30 +10,3 @@ export default {
   },
 };
 
-// More on writing stories with args: https://storybook.js.org/docs/7.0/react/writing-stories/args
-export const Primary = {
-  args: {
-    primary: true,
-    label: 'Button',
-  },
-};
-
-export const Secondary = {
-  args: {
-    label: 'Button',
-  },
-};
-
-export const Large = {
-  args: {
-    size: 'large',
-    label: 'Button',
-  },
-};
-
-export const Small = {
-  args: {
-    size: 'small',
-    label: 'Button',
-  },
-};
